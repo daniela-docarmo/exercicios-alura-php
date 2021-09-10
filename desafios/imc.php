0b01d0cbc1ac87ea8c7d50e66c5314668b59a28c
daniela <danidocarmo9745@gmail.com>
2021-09-10 13:11:19 -0300

    Adicionei condições ao código

--- desafios/imc.php
+++ desafios/imc.php
@@ -6,8 +6,18 @@ class IMC{
      {
         $imc = $peso /($altura * $altura);
 
-         echo "Seu IMC é " . number_format((float)$imc, 2, '.', ''). " Kg/m2" . PHP_EOL;
-           
+         echo "Seu IMC é " . number_format((float)$imc, 2, '.', ''). " Kg/m2. Você está com o IMC ";
+
+         if($imc < 18.5){
+             echo "abaixo ";
+         } else if($imc < 25){
+             echo "dentro "; 
+         } else {
+             echo "acima";
+         }
+
+         echo " do recomendado." . PHP_EOL;
+
      }
 }
 
